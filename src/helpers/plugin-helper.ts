f3300431a9039bce4c58d57e880d510a3a47ff21
Normantas <45922601+NormantasST@users.noreply.github.com>
2026-02-16 18:15:31 +0200

    Add Text Wrapping for Easier Readability (#14)

--- src/helpers/plugin-helper.ts
+++ src/helpers/plugin-helper.ts
@@ -12,3 +12,23 @@ export async function notifyAll()
         }
     });
 }
+
+export function wrapText(text: string, targetLength: number = 9): string {
+    const words = text.split(" ");
+    let output = "";
+    let currentSentenceLength = 0;
+    words.forEach(word => {
+        if (currentSentenceLength + word.length + 1 <= targetLength)
+        {
+            output = output.concat(" ", word);
+            currentSentenceLength += word.length;
+        }
+        else
+        {
+            output = output.concat("\r\n", word);
+            currentSentenceLength = word.length;
+        }
+    });
+
+    return output;
+}
